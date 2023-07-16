finished the implementation and validation of the scaled fot product self attention function as well as a correct implementation of softmax

Functions.py
@@ -30,11 +30,16 @@ def softmax(numpyObject):
         array([0.09003057, 0.24472847, 0.66524096])
     
     """
-    Exponential = np.exp(numpyObject)
-    return Exponential/np.sum(Exponential)
+    # the numpyObject - np.max(numpyObject) is here to avoid infinty
+    # as this function is invariant if offset by a constant
+    
+    # I also noted while debugging that the softmax layer in Kera actually is applied row by row
+    # and not on the whole matrix, this makes sense if you think about how data is passed into machine learning models
+    Exponential = np.exp(numpyObject - np.max(numpyObject, axis=-1, keepdims=True))
+    return Exponential/np.sum(Exponential, axis=-1, keepdims=True)
 
     
-def scaledDotProductSelfAttention(Q,K,V):
+def scaledDotProductSelfAttention(Q,K,V, mask=None):
     """
     Implements scaled dot product self attention.
 
@@ -63,8 +68,44 @@ def scaledDotProductSelfAttention(Q,K,V):
     """
     
     keyDimension = np.shape(K)[-1]
-    Scores = np.dot(Q,K.T)/np.sqrt(keyDimension)
+    Scores = np.dot(Q,K.T)/np.sqrt(float(keyDimension))
 
     weights = softmax(Scores)
 
+    # in the video, they used a tf.where and put the value to -np.inf
+    # I think, that this is better and maybe more efficient, I may be wrong though
+    if mask is not None:
+        weights = weights * mask
+
     return np.dot(weights, V), weights
+
+
+if __name__=="__main__":
+    
+        Queries = np.array([[0.29917916,0.32222616,0.44944202,0.40087818],
+                            [0.42742966,0.75929284,0.94481094,0.16952069],
+                            [0.66734589,0.18528671,0.72916473,0.01845809]])
+        
+        Keys = np.array([[0.36584701,0.43562185,0.4480778 ,0.90873129],
+                         [0.31221214,0.07409408,0.83602278,0.04485871],
+                         [0.87830161,0.9997026 ,0.95931107,0.09707838]])
+        
+        Values = np.array([[0.35764971,0.1772979 ,0.60785344,0.87649764],
+                           [0.07441695,0.98663028,0.18366487,0.38199649],
+                           [0.10817892,0.45987829,0.05264802,0.53601417]])
+
+        ExpectedOutput = np.array([[0.18201628,0.5167383,0.27626652,0.6057456 ],
+                                   [0.1683729 ,0.52207  ,0.24163088,0.5893614 ],
+                                   [0.16761667,0.5406556,0.24742973,0.5844119 ]])
+        
+        ExpectedWeights = np.array([[0.33489865,0.28760365,0.3774977 ],
+                                    [0.277405  ,0.26688248,0.4557126 ],
+                                    [0.27928528,0.30317473,0.41753995]])
+
+        Output, weights = scaledDotProductSelfAttention(Queries, Keys, Values)
+
+        diffOut = np.sum(ExpectedOutput - Output)
+        diffWeights = np.sum(ExpectedWeights - weights)
+
+        print(np.sum(ExpectedOutput - Output))
+        print(np.sum(ExpectedWeights - weights))